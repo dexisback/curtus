a6ae3d22391ecfbce2179795b24778a9c810229d
Amaan <texamaan86@gmail.com>
2026-04-19 23:51:41 +0530

    pivot to studyWithMe

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+
+import SocketPrewarm from "@/components/socket-prewarm";
+
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,6 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <SocketPrewarm />
         {children}
       </body>
     </html>
