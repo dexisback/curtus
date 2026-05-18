66097cdd7744ae3b65ed58e5f8f270fd5b99c9fc
Amaan <texamaan86@gmail.com>
2026-05-18 18:47:02 +0530

    add: testimonials pfp

--- components/testimonial-animated-tooltip.tsx
+++ components/testimonial-animated-tooltip.tsx
@@ -13,45 +13,33 @@ type TestimonialPerson = {
 const people: TestimonialPerson[] = [
   {
     id: 1,
-    name: 'John Doe',
-    designation: 'Software Engineer',
-    image:
-      'https://images.unsplash.com/photo-1599566150163-29194dcaad36?auto=format&fit=crop&w=3387&q=80',
+    name: 'Diptanshu',
+    designation: 'UI/UX designer',
+    image: '/curtus-testimonials/diptanshutsx-pfp.png',
   },
   {
     id: 2,
-    name: 'Robert Johnson',
-    designation: 'Product Manager',
-    image:
-      'https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=800&q=60',
+    name: 'LowkeyDevs',
+    designation: 'Platform Dev',
+    image: '/curtus-testimonials/lowkeydevs-pfp.png',
   },
   {
     id: 3,
-    name: 'Jane Smith',
-    designation: 'Data Scientist',
-    image:
-      'https://images.unsplash.com/photo-1580489944761-15a19d654956?auto=format&fit=crop&w=800&q=60',
+    name: 'Subhraneel',
+    designation: 'FullStack and Agentic Engineer',
+    image: '/curtus-testimonials/subhraneel-pfp.png',
   },
   {
     id: 4,
-    name: 'Emily Davis',
-    designation: 'UX Designer',
-    image:
-      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?auto=format&fit=crop&w=800&q=60',
+    name: 'Vennictus',
+    designation: 'Fullstack Dev',
+    image: '/curtus-testimonials/vennictus-pfp.png',
   },
   {
     id: 5,
-    name: 'Tyler Durden',
-    designation: 'Soap Developer',
-    image:
-      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?auto=format&fit=crop&w=3540&q=80',
-  },
-  {
-    id: 6,
-    name: 'Dora',
-    designation: 'The Explorer',
-    image:
-      'https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?auto=format&fit=crop&w=3534&q=80',
+    name: 'Yash',
+    designation: 'Design | Film ',
+    image: '/curtus-testimonials/yash-pfp.png',
   },
 ];
 
